3998736170521ae7bc8e2616d372a9c57c28652f
pugzillo <lchau91@gmail.com>
2014-07-24 14:26:30 -0400

    fixed errors for input

--- GeneFamily_BlastMCL_pipeline.pl
+++ GeneFamily_BlastMCL_pipeline.pl
@@ -4,8 +4,7 @@ use strict;
 use warnings;
 use Cwd;
 use Getopt::Std;
-use Data::Dumper; 
-
+ 
 ###GeneFamily_BlastMCL_pipeline.pl User notes:
 # 
 #1. Load in fasta file with all the sequences you want to used to find gene femilies (protein or cds)
@@ -16,29 +15,30 @@ use Data::Dumper;
 #sed -e 's/\(^>.*$\)/#\1#/' file.fa | tr -d "\r" | tr -d "\n" | sed -e 's/$/#/' | tr "#" "\n" | sed -e '/^$/d' > linearFile.fa
 
 ###########Input Options#################
-if ($#ARGV != 0 ) {;}
+if ($#ARGV != 2 ) {;}
 my $file = $ARGV[0];  ####Fasta file with sequences you want to use to find gene families 
-my $numProc =$ARGV[1]; #### number of processors to run with
+my $newDir = $ARGV[1]; ####new directory for results
+my $prg = $ARGV[2]; ####what type of blast: blastn or blastp
+
+my $seqtype; 
+
+if ($prg eq "blastn"){
+	$seqtype = "nucl";
+}elsif($prg eq "blastp"){
+	$seqtype = "prot";
+}else{
+	print "NOT VALID BLAST\n";
+	die; 
+}
 
 my $cwd=getcwd();			 #Current directory
-my (%options, )= ();
-my ($seqtype, $prg) = (); 
-my (@blastOUT, @ClusterFiles) = ();
 
-getopts("pn", \%options);	 #enter if protein (-p) or nucleotide (-n)
-
-##takes in user option of protein or nucleotide blast
- if (defined $options{p}){  
- 	$seqtype = "prot"; 
- 	$prg = "blastp"; 
- }else{
- 	$seqtype = "nucl"; 
- 	$prg= "blastn"; 
- }
+####Subroutines
+sub UsageandExit; 
 
 #################Directories##################
 
-my $blastResults = $cwd . "/BLASTfiles"; 
+my $blastResults = $cwd."/".$newDir; 
 mkdir $blastResults;
 
 #######################################BLAST#####################################################
@@ -53,13 +53,16 @@ if (! -e $file.".nhr" && ! -e $file.".nin" && ! -e $file.".nsq"){
 }
 
 #############Blast#############
-my $blastout = $file."blastOUTPUT.txt"; ####File with blast output
+my $blastoutput = $file."blastOUTPUT.txt"; ####File with blast output
 
 ###Checks for the type of BLAST and performs it (eval cutoff og 0.00001)
 if ($prg eq "blastn"){
 	system("blastn -outfmt=6 -evalue=0.00001 -task=blastn -query ".$file." -db ".$file." -out ".$blastoutput.""); 
 }elsif($prg eq "blastp"){
 	system("blastp -outfmt=6 -evalue=0.00001 -task=blastp -query ".$file." -db ".$file." -out ".$blastoutput."");
+}else{
+	print "Not valid blast option!!!!!!\n";
+	die; 
 }
 
 system("mv ".$blastoutput." ".$blastResults.""); ###Move blast result files to the blastoutput directory
@@ -72,7 +75,7 @@ system("cut -f 1,2,11 ".$blastoutput." > ".$blastoutput."MCLinput.txt");
 
 ########################MCL clustering#########################################
 #####Create network and dictionary file; --abc-neg-log10 tranforms the numerical values in the input (the BLAST E-values) by taking the logarithm in base 10 and subsequently negating the sign; the transformed values are capped so that any E-value below 1e-200 is set to a maximum allowed edge weight of 200
-system("mcxload --stream-mirror -abc ".$blastoutput."MCLinput.txt --stream-neg-log10 -stream-tf 'ceil(200)' -o ".$blastoutput."_blastMCL.mci -write-tab ".$blastresult."_blastMCL.tab");
+system("mcxload --stream-mirror -abc ".$blastoutput."MCLinput.txt --stream-neg-log10 -stream-tf 'ceil(200)' -o ".$blastoutput."_blastMCL.mci -write-tab ".$blastoutput."_blastMCL.tab");
 
 ####Clustering; -I is the inflation constant; range from 1.2-5, with 5 having the most fine granularity in clusteringand 1.2 having the largest (check Mammal_ensembl_GeneFams.xlsx for parameter tests)
 system("mcl ".$blastoutput."_blastMCL.mci -I 2.1 -tf 'gq(0.7)' -use-tab ".$blastoutput."_blastMCL.tab");
@@ -81,7 +84,7 @@ system("mcl ".$blastoutput."_blastMCL.mci -I 2.1 -tf 'gq(0.7)' -use-tab ".$blast
 
 sub UsageandExit{
 	print "Usage:\n";
-	print "./GeneFamily_BlastMCL_pipeline.pl -n/-p <fasta.fa> <numProcessors>\n";
+	print "./GeneFamily_BlastMCL_pipeline.pl <fasta.fa> <new directory> <blastp/blastn>\n";
     exit;
 	}
 	
